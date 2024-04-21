88299941e1be3ece641c2e6068153099820b30f0
Rafael <rafaelbento.pro@gmail.com>
2024-04-21 08:17:21 -0300

    dark mode

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,8 @@ function vermais(){
     }
 }
 
-// ESQUERDA DIREITA SEGUE
+// ESQUERDA DIREITA
+
 let myObserver = new IntersectionObserver((entries) => {
     entries.forEach( (entry) => {
         if(entry.isIntersecting) {
@@ -33,3 +34,34 @@ let myObserver = new IntersectionObserver((entries) => {
 let elements = document.querySelectorAll('.leftsegue')
 
 elements.forEach((element) => myObserver.observe(element))
+
+// DARK MODE
+
+const changeThemeBtn = document.querySelector("#change-theme");
+
+// Toggle dark mode
+function toggleDarkMode() {
+  document.body.classList.toggle("dark");
+}
+
+// Load light or dark mode
+function loadTheme() {
+  const darkMode = localStorage.getItem("dark");
+
+  if (darkMode) {
+    toggleDarkMode();
+  }
+}
+
+loadTheme();
+
+changeThemeBtn.addEventListener("change", function () {
+  toggleDarkMode();
+
+  // Save or remove dark mode from localStorage
+  localStorage.removeItem("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("dark", 1);
+  }
+});
